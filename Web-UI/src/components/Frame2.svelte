1ae3f4845ba035ff809cd8b80d4406f1c1974823
Flawid DSouza <me@flawiddsouza.com>
2023-07-07 19:19:22 +0530

    feat(web-ui): Sticky Page Nav (History, Uploads, Styles, Export) for direct page link - until this, the only way to access the page nav actions were through the front page

--- Web-UI/src/components/Frame2.svelte
+++ Web-UI/src/components/Frame2.svelte
@@ -1,5 +1,6 @@
 <script>
 import Page from './Page.svelte'
+import PageNav from './PageNav.svelte'
 import debounce from '../helpers/debounce.js'
 import fetchPlus from '../helpers/fetchPlus.js'
 
@@ -36,14 +37,33 @@ getPageInfo()
 {#if pageNotFound}
     <div style="display: grid; place-items: center; height: 100vh; font-size: 2rem;">404 Page Not Found</div>
 {:else}
-    <Page activePage={activePage} updatePageName={updatePageName} className="journal-page-container"></Page>
+    <div style="display: grid; grid-template-rows: auto 1fr; height: 100vh;">
+        <div class="page-header">
+            <div style="margin-left: 2em">
+                {#if activePage.locked === false && activePage.type !== 'PageGroup'}
+                    <PageNav bind:activePage={activePage}></PageNav>
+                {/if}
+            </div>
+        </div>
+        <Page activePage={activePage} updatePageName={updatePageName} className="journal-page-container"></Page>
+    </div>
 {/if}
 
 <style>
 :global(.journal-page-container) {
-    height: 100vh;
+    height: 100%;
     overflow-y: auto;
     padding-left: 2rem;
-    padding-top: 2rem;
+    padding-top: 1.4em;
+}
+
+.page-header {
+    user-select: none;
+    font-size: 1.2em;
+    padding-top: 0.5em;
+    padding-right: 1em;
+    padding-bottom: 0.6em;
+    border-bottom: 1px solid lightgrey;
+    background-color: white;
 }
 </style>
