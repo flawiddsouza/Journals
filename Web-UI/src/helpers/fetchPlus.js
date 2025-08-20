4004afc1f02ca83fc3b73e45a8a291023efd2c23
Flawid D'Souza <me@flawiddsouza.com>
2025-08-21 02:23:27 +0530

    feat(web-ui): refreshing token reloads the window, which is not ideal - now we refresh the token and immediately retry the failed request - no more sudden page reloads halfway through typing into a page

--- Web-UI/src/helpers/fetchPlus.js
+++ Web-UI/src/helpers/fetchPlus.js
@@ -10,16 +10,27 @@ const fetchPlus = function(method, url, data, headers = {}) {
         headers: Object.assign({}, fetchPlus.headers, headers)
     }).then(res => res.ok ? res.json() : Promise.reject(res)).catch(res => {
         if(res.status === 401) { // if response code is unauthorized
-            fetchPlus.post('/login', {
+            // Try to refresh token
+            return fetchPlus.post('/login', {
                 username: localStorage.getItem('username'),
                 password: localStorage.getItem('password')
             }).then(response => {
                 if(response.hasOwnProperty('error')) {
                     // if there's an error, perform logout + avoid infinite loop
                     logoutAccount()
+                    return Promise.reject(res)
                 } else {
                     localStorage.setItem('token', response.token)
-                    location.reload()
+                    fetchPlus.token = response.token
+                    // Update token in headers
+                    fetchPlus.headers['Token'] = response.token
+                    // Retry the original request with new token
+                    return fetch(baseURL + url, {
+                        method: method.toUpperCase(),
+                        body: JSON.stringify(data),
+                        credentials: fetchPlus.credentials,
+                        headers: Object.assign({}, fetchPlus.headers, headers)
+                    }).then(res2 => res2.ok ? res2.json() : Promise.reject(res2))
                 }
             })
         }
